ebb24ea5a6827b9ba5f10d7a51abe27957b2047d
Caleb <caleb@Jennys-MacBook-Air.local>
2021-08-09 22:34:04 -0400

    fixing a bootstrap issue and also adding css styling and container row and col to break up main comment section

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import './comment.css'
 
 export const Comment = () => {
   return (
-    <div className='comment'>
-      <p>This is a test</p>
-      <p>This is a test</p>
-    </div>
+    <Container className='comment'>
+      <Row>
+        <Col sm={2} className='comment-section-upper'>
+          <img src="caleb_quinn.png" className='profile-picture'></img>
+        </Col>
+        <Col sm={10}>
+          <textarea className="form-control status-box text-box" placeholder="What is on your mind?"></textarea>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='comment-section-lower'>section two</Col>
+      </Row>
+    </Container>
   )
 }
