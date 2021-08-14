2c4d5211faf88b89f79383e065ad5c6cb0905cd3
calebq2010 <calebq2010@gmail.com>
2021-08-14 10:34:40 -0400

    updating index file to use provider and the store I created

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './app/App';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
