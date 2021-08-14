e06da6005681b741d6ca5f958058b22bebbe851d
calebq2010 <calebq2010@gmail.com>
2021-08-14 18:07:11 -0400

    fixing if you hit post it when nothing is entered it will not post an empty post to the store

--- src/features/posts/MainPostSection.tsx
+++ src/features/posts/MainPostSection.tsx
@@ -13,8 +13,10 @@ export const MainPostSection:FC<MainPostProps> = ({ addPost }: MainPostProps) =>
   const onEnterKeyPressed = (event: React.KeyboardEvent<HTMLInputElement>) => {event.key === 'Enter' && event.preventDefault()}
 
   const onPostItClick = () => {
-   addPost(textValue)
-   setTextValue("")
+    if(textValue){
+      addPost(textValue)
+      setTextValue("")
+    }
   }
 
   return (
