f9407e1ba23d089fb145dd20d4cc5e13306639b2
calebq2010 <calebq2010@gmail.com>
2021-08-15 17:49:20 -0400

    adding test to make sure you are able to click the like button

--- tests/facebook-post.js
+++ tests/facebook-post.js
@@ -22,14 +22,22 @@ describe('facebook post tests', function() {
         const postValue = 'This is Calebs test input from nightwatch!'
 
         browser
-            .assert.visible('input[type=text')
+            .assert.visible('input[type=text]')
             .setValue('input[type=text]', postValue)
             .assert.visible('button[data-testid="post-btn"]')
             .click('button[data-testid="post-btn"]')
             .assert.visible('section[data-testid="post"]')
             .assert.containsText('p[data-testid="post-value"]', postValue)
-            .pause(2000)
     })
+
+    test('Confirm I can like a post', function (browser) {
+
+      browser
+          .assert.visible('div[data-testid="like-btn"')
+          .click('div[data-testid="like-btn"')
+          .assert.containsText('span[data-testid="post-likes"]', '1 Likes')
+          .pause(2000)
+  })
   
     after(browser => browser.end());
   });
